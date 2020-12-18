fe6ddb50a39230237b695bbc44a8b6984d22d2cc
eaccmk (Millan kaul) <eaccmk@gmail.com>
2020-12-18 14:36:33 +1100

    Adding a URL request to run from CI

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log("Wow this works !");
+
+const fetch = require("node-fetch");
+
+callEndpointAndFetresponse();
+
+function callEndpointAndFetresponse() {
+    /* 
+    Actual utl : https://gist.github.com/my-fitbit/afcc19876d87c41c0401f07184bb5b44#file-200-json
+    using raw url to get jsson response in fetch
+    */
+    fetch("https://gist.githubusercontent.com/my-fitbit/afcc19876d87c41c0401f07184bb5b44/raw/c7977e2873fbd77003929369a383d592cbadb8f3/200.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            // convert JSON n Response string
+            let responseData = JSON.stringify(response);
+
+            console.log("\n Response body recieved is : " + responseData);
+
+        })
+        .catch(function (error) { console.log(error); });
+}
